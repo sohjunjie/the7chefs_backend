530267cb30c8f14bcab5699c5b45add669faf368
sohjunjie <junjie.soh93@gmail.com>
2017-09-06 00:12:23 +0800

    remove comments

--- sevchefs_api/urls.py
+++ sevchefs_api/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from sevchefs_api import views
 
 urlpatterns = [
-    # url(r'api/v1.0/comment/recipe/(?P<pk>[0-9]+)/$', views.CommentRecipeView, name="comment-recipe"),
     url(r'api/v1.0/comment/recipe/(?P<pk>[0-9]+)/$', views.CommentRecipeView.as_view(), name="comment-recipe"),
 ]

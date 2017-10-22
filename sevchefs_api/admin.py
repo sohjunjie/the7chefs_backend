6f57deacc468ffb3606b92d14865353d2ac9548d
sohjunjie <junjie.soh93@gmail.com>
2017-10-22 14:22:53 +0800

    add recipe admin

--- sevchefs_api/admin.py
+++ sevchefs_api/admin.py
@@ -7,4 +7,10 @@ class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'image')
 
 
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    list_display = ('name', 'description', 'upload_datetime', 'upload_by_user', 'image', 'difficulty_level')
+
+
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)

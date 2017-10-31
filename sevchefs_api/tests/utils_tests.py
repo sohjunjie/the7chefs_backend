4f4ca2cba1d996a0b2b4ad71e9da1d12c7fdec79
sohjunjie <junjie.soh93@gmail.com>
2017-10-31 14:28:31 +0800

    add utils test

--- sevchefs_api/tests/utils_tests.py
+++ sevchefs_api/tests/utils_tests.py
@@ -1,11 +1,23 @@
 from sevchefs_api.tests import base_tests
 from sevchefs_api.utils import *
-from sevchefs_api.models import RecipeTag
+from sevchefs_api.models import RecipeTag, Ingredient
+
+from rest_framework.exceptions import NotFound
 
 
 class UtilsTests(base_tests.BaseApiTest):
 
     def test_get_recipe_tag_or_none_return_none(self):
         non_exist_recipe_tag_id = 1
-        recipe_tag = RecipeUtils.get_recipe_tag_or_none(1)
+        recipe_tag = RecipeUtils.get_recipe_tag_or_none(non_exist_recipe_tag_id)
         self.assertIsNone(recipe_tag)
+
+    def test_get_ingredient_or_404_raise_404(self):
+        non_exist_ingredient_id = 1
+        self.assertRaises(NotFound, RecipeUtils.get_ingredient_or_404,
+                          non_exist_ingredient_id)
+
+    def test_get_recipe_instruction_or_404_raise_404(self):
+        non_exist_recipe_instruction_id = 1
+        self.assertRaises(NotFound, RecipeUtils.get_recipe_instruction_or_404,
+                          non_exist_recipe_instruction_id)

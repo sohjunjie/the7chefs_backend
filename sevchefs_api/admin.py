edeb357b29f5e6db0330cf5fee60fd0bed4f373c
sohjunjie <junjie.soh93@gmail.com>
2017-11-01 19:49:04 +0800

    add admin control for many models

--- sevchefs_api/admin.py
+++ sevchefs_api/admin.py
@@ -12,5 +12,28 @@ class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'upload_datetime', 'upload_by_user', 'image', 'difficulty_level')
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    model = UserProfile
+    list_display = ('user', 'description', 'avatar')
+
+
+class ActivityTimelineAdmin(admin.ModelAdmin):
+    model = ActivityTimeline
+    list_display = ('user', 'summary_text', 'target_user', 'main_object_image', 'target_object_image', 'datetime')
+
+
+class RecipeCommentAdmin(admin.ModelAdmin):
+    model = RecipeComment
+    list_display = ('recipe', 'user', 'text', 'datetime')
+
+
+class RecipeTagAdmin(admin.ModelAdmin):
+    model = RecipeTag
+    list_display = ('text',)
+
+
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(ActivityTimeline, ActivityTimelineAdmin)
+admin.site.register(RecipeComment, RecipeCommentAdmin)
